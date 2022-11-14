7f5c61bcfa31420fa1e7cfbf2381111887f83b51
Niklas Kemper <65356992+Niklas-23@users.noreply.github.com>
2022-11-14 12:19:18 +0100

    Update pyterrier basic model

--- src/pyterrier/pyterrier_basic_model.py
+++ src/pyterrier/pyterrier_basic_model.py
@@ -1,10 +1,15 @@
 from typing import Tuple, Union, List
 
+from pandas import DataFrame
+import pandas as pd
+
 from arqmath_code.Entities.Post import Answer, Question
 from arqmath_code.topic_file_reader import Topic
 from src.base.model import Model
 from src.pyterrier.pyterrier_index import create_pyterrier_index
 import pyterrier as pt
+
+from src.pyterrier.pyterrier_math_formula_coding import translate_latex
 
 
 class BasicPyTerrierModel(Model):
@@ -16,11 +21,32 @@ class BasicPyTerrierModel(Model):
 
     def forward(self, queries: List[Topic], documents: List[Union[Question, Answer]]) -> List[
         Tuple[Topic, Union[Question, Answer], float]]:
+
         if type(documents) is List[Question]:
-            raise Exception("Question can't be index using the basic PyTerrier model")
+            raise Exception("Question can't be indexed using the basic PyTerrier model")
+
+        if not pt.started():
+            pt.init()
+
         self.pyterrier_index = create_pyterrier_index(documents=documents, index_name="pyterrier_answer_index_task1_v1")
+
         batch_retrieve = pt.BatchRetrieve(self.pyterrier_index, wmodel=self.pyterrier_model)
-        ranking = batch_retrieve.search(queries)
+        queries_data_frame = self.get_pyterrier_query_dict(queries)
+        ranking: DataFrame = batch_retrieve.transform(queries_data_frame)
         documents_to_include = set([answer.post_id for answer in documents])
-        ranking = ranking[ranking.docno in documents_to_include]
-        return ranking
+        result = []
+        for index, row in ranking.iterrows():
+            post_id = int(row["docno"])
+            if post_id in documents_to_include:
+                topic = next((topic for topic in queries if topic.topic_id == row["qid"]), None)
+                answer = next((answer for answer in documents if answer.post_id == post_id), None)
+                result.append((topic, answer, row["score"]))
+
+        return result
+
+    def get_pyterrier_query_dict(self, queries: List[Topic]):
+        query_dict = []
+        for topic in queries:
+            query_dict.append({'qid': topic.topic_id, 'query': translate_latex(topic.question)})
+        queries_data_frame = pd.DataFrame(query_dict)
+        return queries_data_frame
--- src/pyterrier/pyterrier_index.py
+++ src/pyterrier/pyterrier_index.py
@@ -2,20 +2,18 @@ import os
 from typing import List
 
 import pyterrier as pt
-from pyterrier.index import IterDictIndexer, IndexRef
 
 from arqmath_code.Entities.Post import Answer
-from arqmath_code.post_reader_record import DataReaderRecord
 from src.pyterrier.pyterrier_math_formula_coding import *
 from src.pyterrier.config import ROOT_DIRECTORY
 
 dict_field_names = ("docno", "text", "origtext", "parentno", "votes")
 
-def get_arqmath_answers_as_iterable(data_reader: DataReaderRecord):
-    for answer in data_reader.get_all_answer_posts():
+
+def get_pyterrier_answer_dict(documents: List[Answer]):
+    for answer in documents:
         if answer.body is not None and answer.post_id is not None and answer.parent_id is not None and answer.votes is not None:
             indexed_body = translate_latex(answer.body)
-            #tags = data_reader.get_tags_for_answer_by_id(answer.parent_id) ignore tags for now since they lead to key errors
 
             yield {'docno': answer.post_id,
                    'text': indexed_body,
@@ -26,13 +24,16 @@ def get_arqmath_answers_as_iterable(data_reader: DataReaderRecord):
 
 
 def create_pyterrier_index(documents: List[Answer], index_name: str = "arqmath_indexV1"):
-    index_path: str = f"{ROOT_DIRECTORY}./index/{index_name}"
+    index_path = f"{ROOT_DIRECTORY}/index/{index_name}"
+    print(index_path)
+    if not pt.started():
+        pt.init()
     if not os.path.exists(index_path + "/data.properties"):
-        indexer: IterDictIndexer = pt.IterDictIndexer(index_path, overwrite=True)
-        index_reference: IndexRef = indexer.index(documents)
+        indexer = pt.index.IterDictIndexer(index_path)
+        index_reference = indexer.index(get_pyterrier_answer_dict(documents))
         pass
     else:
-        index_reference: IndexRef = pt.IndexRef.of(index_path + "/data.properties")
+        index_reference = pt.IndexRef.of(index_path + "/data.properties")
         pass
 
     index = pt.IndexFactory.of(index_reference)
