6c19083f61dda81e4328af02e450b375ae78b979
Niklas Kemper <65356992+Niklas-23@users.noreply.github.com>
2022-11-14 16:46:26 +0100

    Improve model performance

--- src/pyterrier/pyterrier_basic_model.py
+++ src/pyterrier/pyterrier_basic_model.py
@@ -11,9 +11,10 @@ import pyterrier as pt
 
 class BasicPyTerrierModel(Model):
 
-    def __init__(self, pyterrier_model: str = "TF_IDF"):
+    def __init__(self, pyterrier_model: str = "TF_IDF", pyterrier_index_name: str = "pyterrier_answer_index_task1_v1"):
         self.pyterrier_index = None
         self.pyterrier_model = pyterrier_model
+        self.pyterrier_index_name = pyterrier_index_name
         pass
 
     def forward(self, queries: List[Topic], documents: List[Union[Question, Answer]]) -> List[
@@ -25,18 +26,21 @@ class BasicPyTerrierModel(Model):
         if not pt.started():
             pt.init()
 
-        self.pyterrier_index = create_pyterrier_index(documents=documents, index_name="pyterrier_answer_index_task1_v1")
+        self.pyterrier_index = create_pyterrier_index(documents=documents, index_name=self.pyterrier_index_name)
 
         batch_retrieve = pt.BatchRetrieve(self.pyterrier_index, wmodel=self.pyterrier_model)
         queries_data_frame = get_pyterrier_query_dict(queries)
         ranking: DataFrame = batch_retrieve.transform(queries_data_frame)
+
         documents_to_include = set([answer.post_id for answer in documents])
+        documents_dict = {answer.post_id: answer for answer in documents}
+        queries_dict = {topic.topic_id: topic for topic in queries}
+
         result = []
         for index, row in ranking.iterrows():
-            post_id = int(row["docno"])
-            if post_id in documents_to_include:
-                topic = next((topic for topic in queries if topic.topic_id == row["qid"]), None)
-                answer = next((answer for answer in documents if answer.post_id == post_id), None)
+            if int(row["docno"]) in documents_to_include:
+                topic = queries_dict[row["qid"]]
+                answer = documents_dict[int(row["docno"])]
                 result.append((topic, answer, row["score"]))
 
         return result
